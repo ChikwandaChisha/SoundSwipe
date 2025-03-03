155d79d3f2efd134f4e6321a59b8eaf8bfc386f9
Maddy Slater <156967232+maddyslater@users.noreply.github.com>
2025-03-03 13:51:56 -0500

    basic tile

--- mobile-app/components/playlist_tile.jsx
+++ mobile-app/components/playlist_tile.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Pressable, Icon } from 'react-native';
 // import Animated, {
 //   interpolate,
 //   useAnimatedStyle,
 //   useSharedValue,
 //   withTiming,
 // } from "react-native-reanimated";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faPlus, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+// import { faPlus, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 const PlaylistTile = ({
     title,
@@ -17,36 +17,45 @@ const PlaylistTile = ({
 
   const [addedPlaylists, setAddedPlaylists] = useState({});
 
-  const togglePlaylist = (id) => {
+  const togglePlaylist = (item) => {
     setAddedPlaylists((prevState) => ({
       ...prevState,
-      [id]: !prevState[id],
+      [playlist]: !prevState[playlis],
     }));
+  };
+
+  const addSong = ([playlists]) => {
+    //add song to playlists through API here
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.card}>
         <View style={styles.currentSong}>
-          <Image source={{uri:profilePic}} style={styles.profilePic} />
+          <Image source={"https://cdn-icons-png.freepik.com/512/5997/5997002.png"} style={styles.profilePic} />
+          <View style={styles.songText}>
             <Text style={styles.songTitle}>{title}</Text>
             <Text style={styles.songArtist}>{artist}</Text>
+           </View>
         </View>
-        <Text>Add to Playlist</Text>
-        <View>
+        <View style={styles.addSong}>
+          <Text>Add to Playlist</Text>
+          <View style={styles.playlistList}>
           {playlists.map((playlist, index)=> (
             <View key={index} style={styles.playlistContainer}>
               <Image></Image>
               <Text>Playlist Title</Text>
-              <TouchableOpacity onPress={() => toggleSong(item.id)}>
-              <Icon
+              <TouchableOpacity onPress={() => togglePlaylist(playlist)}>
+                <Text>Add</Text>
+              {/* <Icon
                   name={addedPlaylists[playlist.id] ? 'check' : 'plus'}
                   size={24}
                   color={addedPlaylists[playlist.id] ? 'green' : 'black'}
-                />
+                /> */}
               </TouchableOpacity>
-            </View>
+          </View>
           ))}
+        </View>
         </View>
       </View>
 
@@ -56,15 +65,40 @@ const PlaylistTile = ({
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
   card: {
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
     width: 300,
-    height: 400,
+    height: 500,
+    borderRadius: 20,
     backgroundColor: '#d68e47',
   },
   currentSong: {
     width: 200,
-    height: 75,
+    height: 100,
+    borderRadius: 10,
     backgroundColor: 'white',
+    padding: 15,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  songText: {
+    fontSize: 10,
+  },
+  addSong: {
+    width: 225,
+    height: 300,
+    borderRadius: 10,
+    backgroundColor: 'green',
+    padding: 25,
+    alignItems: 'center',
+  },
+  playlistContainer: {
+    flexDirection: 'row',
   }
 
 });
